413b4037633c32cdd91226ed0a7a5c83fcd56b90
Elizabeth G. Schmitz <egs27@pitt.edu>
2019-10-25 15:02:21 -0400

    adding data and showing data table

--- git_exercise.R
+++ git_exercise.R
@@ -1,1 +1,5 @@
 ## Practice 2.2A tIGER THINGY for git purposes
+
+## Adding Tiger Data
+tigerData <- read.csv(url("http://whitlockschluter.zoology.ubc.ca/wp-content/data/chapter02/chap02e2aDeathsFromTigers.csv"))
+head(tigerData)
