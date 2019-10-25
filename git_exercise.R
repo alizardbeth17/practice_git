9579e7ba4551fdd37f9663145b8a14094fdeaf4c
Elizabeth G. Schmitz <egs27@pitt.edu>
2019-10-25 15:03:12 -0400

    making frequency table

--- git_exercise.R
+++ git_exercise.R
@@ -3,3 +3,8 @@
 ## Adding Tiger Data
 tigerData <- read.csv(url("http://whitlockschluter.zoology.ubc.ca/wp-content/data/chapter02/chap02e2aDeathsFromTigers.csv"))
 head(tigerData)
+
+## Making Frequency Table
+
+tigerTable <- sort(table(tigerData$activity), decreasing = TRUE)
+tigerTable
